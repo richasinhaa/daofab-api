311e31db3983d96cc3fc6d57f1353c86cb8985fa
richasinhaa <richa.sinha0603@gmail.com>
2021-07-29 15:33:50 +0800

    First commit

---
+++ src/main/java/com/api/daofab/DaofabApplication.java
@@ -0,0 +1,13 @@
+package com.api.daofab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaofabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaofabApplication.class, args);
+	}
+
+}
